a9b7839c923eb780b84be25695a7407a85a7cb69
dandyraka <dandyraka13@gmail.com>
2020-04-15 15:52:14 +0700

    Update

--- src/aksi.js
+++ src/aksi.js
@@ -1,11 +1,14 @@
 function ngganti(){
-    var i, status, matches, ganti, elem, push;
+    var i, status, matches, ganti, elem, push, rgx;
     elem = document.getElementsByTagName("p");
+    rgx = "(\s+|\*|#|,|-|\(.\)|\[.\]|\{.\}|\s+.\s+|.\s+|\s+.)";
     for (i=0; i<elem.length; i++) {
         status = elem[i].textContent;
-        matches = status.match(/(https|http)\:\/\/(.*?)(\*|#|,|-|\(.\)|\[.\]|\{.\}|\s+.\s+|.\s+|\s+.)(.*)/g);
+        matches = status.match(/(https|http)\:\/\/(.*?)(\s+|\*+|#+|,+|-+|\(dot\)|\(.\)|\[.\]|\{.\}|\s+\.\s+|\.\s+|\s+\.)(.*)/g);
         if(matches){
-            ganti = matches[0].replace(/\*|#|,|-|\(.\)|\[.\]|\{.\}|\s+\.\s+|\.\s+|\s+\./, ".");
+            ganti = matches[0].replace(/\s+|\*+|#+|,+|-+|\(dot\)|\(.\)|\[.\]|\{.\}|\s+\.\s+|\.\s+|\s+\./, ".").trim();
+            ganti = ganti.replace(/\.+/, ".");
+            //ganti = ganti.replace(/\.(?=[^.]*$)/, ' ');
             push = status.replace(matches, '<a href="' + ganti + '" target="_blank">' + ganti + '</a>');
             elem[i].innerHTML = push;
         }
