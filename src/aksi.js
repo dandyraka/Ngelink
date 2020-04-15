3735e0f447a6b9db21d06b6420e0f79a743be680
dandyraka <dandyraka13@gmail.com>
2020-04-15 13:27:17 +0700

    Update

--- src/aksi.js
+++ src/aksi.js
@@ -1,18 +1,17 @@
 function ngganti(){
-    var i, status, matches, ganti, elem;
+    var i, status, matches, ganti, elem, push;
     elem = document.getElementsByTagName("p");
     for (i=0; i<elem.length; i++) {
         status = elem[i].textContent;
         matches = status.match(/(https|http)\:\/\/(.*?)(\*|#|,|-|\(.\)|\[.\]|\{.\}|\s+.\s+|.\s+|\s+.)(.*)/g);
         if(matches){
-            ganti = status.replace(/\*|#|,|-|\(.\)|\[.\]|\{.\}|\s+\.\s+|\.\s+|\s+\./, ".");
-            elem[i].innerHTML = '<a href="' + ganti + '" target="_blank">' + ganti + '</a>';
+            ganti = matches[0].replace(/\*|#|,|-|\(.\)|\[.\]|\{.\}|\s+\.\s+|\.\s+|\s+\./, ".");
+            push = status.replace(matches, '<a href="' + ganti + '" target="_blank">' + ganti + '</a>');
+            elem[i].innerHTML = push;
         }
     }
 }
 ngganti();
-$(window).scroll(function() {
-    if($(window).scrollTop() + $(window).height() == $(document).height()) {
-        ngganti();
-    }
- });
+window.onscroll = function (e) {
+    ngganti();
+}
